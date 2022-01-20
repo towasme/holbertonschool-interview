c2937769a8214da75b4a25480069f53e2c4d1b49
tomas.estrada <tomas.estrada@rappi.com>
2022-01-20 15:01:43 -0500

    fixing but still ati it

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -1,136 +1,60 @@
-#include "sort.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "sort.h"
 
 
 /**
- * PrintArray - prints array of integers for range of indicies
- * @array: array of values to be printed
- * @iBeg: starting index value
- * @iEnd: ending index value
- */
-void PrintArray(int *array, int iBeg, int iEnd)
-{
-	int i;
-
-	if (!array)
-		return;
-
-	for (i = iBeg; i < iEnd; i++)
-		if (i < iEnd - 1)
-			printf("%i, ", array[i]);
-		else
-			printf("%i\n", array[i]);
-}
-
-
-/**
- * CopyArray - simple 1 for 1 copy of source[] to dest[]
- * @source: array of values to be sorted
- * @iBeg: starting index value
- * @iEnd: ending index value
- * @dest: array to store sorted integers
- */
-void CopyArray(int *source, int iBeg, int iEnd, int *dest)
-{
-	int i;
-
-	if (!source || !dest)
-		return;
-
-	for (i = iBeg; i < iEnd; i++)
-		dest[i] = source[i];
-}
-
-
-/**
- * TopDownMerge - sorts subsections ("runs") of source[] by ascending value
- * @source: array of values to be sorted
- * @iBeg: left run starting index value
- * @iMid: right run starting index value
- * @iEnd: right run ending index value
- * @dest: array to store sorted integers
- */
-void TopDownMerge(int *source, int iBeg, int iMid, int iEnd, int *dest)
-{
-	int i, j, k;
-
-	if (!source || !dest)
-		return;
-
-	i = iBeg, j = iMid;
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	PrintArray(source, iBeg, iMid);
-	printf("[right]: ");
-	PrintArray(source, iMid, iEnd);
-	/* While there are elements in the left or right runs... */
-	for (k = iBeg; k < iEnd; k++)
-	{
-		/* If left run head exists and is <= existing right run head */
-		if (i < iMid && (j >= iEnd || source[i] <= source[j]))
-		{
-			dest[k] = source[i];
-			i++;
-		}
-		else
-		{
-			dest[k] = source[j];
-			j++;
-		}
-	}
-	printf("[Done]: ");
-	PrintArray(dest, iBeg, iEnd);
-}
-
-
-/**
- * TopDownSplitMerge - recursive engine of merge_sort, splits working copy of
- * array into left and right runs, then merges with TopDownMerge
- * @source: array of integers to be sorted
- * @iBeg: starting index value
- * @iEnd: ending index value
- * @dest: array to store sorted integers
- */
-void TopDownSplitMerge(int *source, int iBeg, int iEnd, int *dest)
-{
-	int iMid;
-
-	if (!source || !dest)
-		return;
-
-	if (iEnd - iBeg < 2) /* if run size == 1 */
-		return;     /* consider it sorted */
-	/* split the run longer than 1 item into halves */
-	iMid = (iEnd + iBeg) / 2;
-
-	TopDownSplitMerge(dest, iBeg, iMid, source);  /* sort left run */
-	TopDownSplitMerge(dest, iMid, iEnd, source);  /* sort right run */
-	/* merge the resulting runs from array[] into work_copy[] */
-	TopDownMerge(source, iBeg, iMid, iEnd, dest);
-}
-
-
-/**
- * merge_sort - sorts an array of integers in ascending order using a
- * top-down merge sort algorithm
- * @array: array of integers to be sorted
- * @size: amount of elements in array
+ * merge_sort - use merge sort in place on an array of integers
+ * @array: array to sort
+ * @size: size of array
  */
 void merge_sort(int *array, size_t size)
 {
-	int *work_copy;
+	int *left, *right, *start = array, top;
+	size_t lsize = size / 2, rsize = size / 2 + size % 2;
+	static int *buffer, *alloc, idx, i;
 
-	if (!array || size < 2)
+	if (array == NULL || size <= 1)
 		return;
-
-	work_copy = malloc(sizeof(int) * size);
-	if (!work_copy)
-		return;
-
-	CopyArray(array, 0, size, work_copy);
-	TopDownSplitMerge(work_copy, 0, size, array);
-
-	free(work_copy);
+	top = 0;
+	if (buffer == NULL)
+	{
+		top = 1;
+		buffer = malloc(sizeof(int) * size);
+	}
+	alloc = buffer;
+	merge_sort(array, lsize);
+	merge_sort(array + lsize, rsize);
+	left = array;
+	right = array + lsize;
+	printf("Merging...\n[left]: ");
+	print_array(left, lsize);
+	printf("[right]: ");
+	print_array(right, rsize);
+	for (idx = 0; lsize > 0 && rsize > 0; idx++)
+	{
+		if (*left < *right)
+		{
+			buffer[idx] = *left;
+			left++;
+			lsize--;
+		}
+		else
+		{
+			buffer[idx] = *right;
+			right++;
+			rsize--;
+		}
+	}
+	idx--;
+	for (i = idx + 1; lsize; lsize--, left++, i++)
+		array[i] = *left;
+	for (i = idx + 1; rsize; rsize--, right++, i++)
+		array[i] = *right;
+	for (; idx >= 0; idx--)
+		array[idx] = buffer[idx];
+	printf("[Done]: ");
+	print_array(start, size);
+	if (top)
+		free(alloc);
 }
